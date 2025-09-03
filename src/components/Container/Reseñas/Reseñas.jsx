3ad9f84adb93107f6c0a99c4f7eb534dc8185ef0
Franco Carranza <francocarranza144@gmail.com>
2025-09-03 01:45:07 -0300

    Cambiando estilos css reseñas

--- src/components/Container/Reseñas/Reseñas.jsx
+++ src/components/Container/Reseñas/Reseñas.jsx
@@ -3,7 +3,6 @@ import "./Reseñas.css";
 function Reseñas() {
   return (
     <div className="reseñas">
-      {/* <h2 className="titulo-reseñas">Opiniones de Google</h2> */}
       <div
         className="elfsight-app-fbb7416b-ebc2-4d7d-9e8f-fb57ceccb9a3"
         data-elfsight-app-lazy
