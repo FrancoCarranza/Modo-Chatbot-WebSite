eb8d90d38672d3ef5a543d0b4197128437dba054
Franco Carranza <francocarranza144@gmail.com>
2025-09-07 17:21:31 -0300

    Arreglo NavBar

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,4 +1,4 @@
-import NavBar from "../NavBar/NavBar";
+import NavBar from "../NavBar/NavBar"
 import Inicio from "./Inicio/Inicio";
 import Servicios from "./Servicios/Servicios";
 import Nosotros from "./Nosotros/Nosotros";
